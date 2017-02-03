9c869aac47587706a3a2c25f9106102ac9a9f4b7
Ziyan <jaraidaniel@gmail.com>
2017-02-04 00:32:56 +0100

    Unify rootdir filenames (2/2)

--- device.mk
+++ device.mk
@@ -1,12 +1,9 @@
 # USB config contains product-specific USB id
 PRODUCT_COPY_FILES += \
-    device/acer/a700/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc
+    device/acer/a700/prebuilt/ramdisk/init.picasso.usb.rc:root/init.picasso.usb.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
-
-# So init files are copied properly in device_base.mk
-PRODUCT_BOOTLOADER := picasso_mf
 
 # Inherit t30-common
 $(call inherit-product, device/acer/t30-common/device_base.mk)
