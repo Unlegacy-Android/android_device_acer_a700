b4941ff3a7d76f90b5c98976e2122281bf5bf629
Ziyan <jaraidaniel@gmail.com>
2017-03-04 14:10:20 +0100

    Remove kernel config, add a510 to TARGET_OTA_ASSERT_DEVICE
    Change-Id: I6f41158043ae62deaacadb2dd4d2eeffbc351e95

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,5 @@
 TARGET_BOOTLOADER_BOARD_NAME := picasso_mf
-TARGET_OTA_ASSERT_DEVICE := picasso_mf,a700_emea_cus1
-
-TARGET_KERNEL_CONFIG := picasso_mf_defconfig
+TARGET_OTA_ASSERT_DEVICE := a700,picasso_mf,a700_emea_cus1
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a700/bluetooth
 
